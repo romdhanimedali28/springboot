77a500473016579cbea361b73c267c417186d48f
romdhanimedali <romdhanimohamedali.28@gmail.com>
2025-03-04 02:58:05 +0100

    init project

---
+++ src/main/java/com/example/learnproject/LearnprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnproject/LearnprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
